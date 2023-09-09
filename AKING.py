d5a1fe0e96ffcefef979bb7225a7b3e3b32fe133
Mr David <144300751+Mr-David-404@users.noreply.github.com>
2023-09-09 23:24:56 +0600

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,20 @@
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import Dump
+elif bit=='33bit':
+    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import Dump32
+else:
+    print(f'{red}[×] Sorry System Not Support{white}')￼Enter
